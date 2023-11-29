f3d46201148c207885caed17d03057ca1e505816
RyuuKodex <dan_kloda@wp.pl>
2023-11-29 13:19:48 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+export default function App() {
+
+  return (
+    <>
+      <div className="container vh-100 d-flex align-items-center justify-content-center">
+        <div className="text-center">
+          <h1 className="display-1">Daniel Kłoda</h1>
+          <span className="fs-3">Software Developer</span>
+
+          <div className="d-flex w-100 justify-content-center mt-5 fs-3">
+            <a className="d-block mx-3" href="https://m.me/RyuuKodex">
+              <i className="fab fa-facebook-messenger"></i>
+            </a>
+            <a className="d-block mx-3" href="https://www.linkedin.com/in/daniel-kłoda-683423258/">
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a className="d-block mx-3" href="https://github.com/RyuuKodex">
+              <i className="fab fa-github"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
